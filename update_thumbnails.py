5ff8a9d4b0716fcb62226487c6b8fd480fec2bd8
Tomas Kozelek <tomas.kozelek@gmail.com>
2023-12-14 09:53:53 +0100

    Fixed update thumbnail script.

--- update_thumbnails.py
+++ update_thumbnails.py
@@ -9,20 +9,22 @@ os.mkdir("public/images/thumbnails")
 
 # get all the jpg files from the current folder
 for filepath in glob.glob("public/images/photos/*"):
-  filename= filepath.split("/")[-1]
+  filename = filepath.split("/")[-1]
   if not os.path.isfile("public/images/thumbnails/" + filename):
        im = Image.open(filepath)
        root_ext = os.path.splitext(os.path.basename(filename))
-       if root_ext[1] != "jpg":
-           rgb_im = im.convert("RGB")
-
+       if root_ext[1] != ".jpg":
            old_filepath = filepath
            filename = root_ext[0] + ".jpg"
            filepath = "public/images/photos/" + filename
 
-           rgb_im.save("public/images/photos/" + root_ext[0]+".jpg")
-           im = rgb_im
-           os.remove(old_filepath)
+           if root_ext[1] in (".JPG", ".jpeg", ".JPEG"):
+               os.rename(old_filepath, filepath)
+           else:
+               rgb_im = im.convert("RGB")
+               rgb_im.save(filepath)
+               im = rgb_im
+               os.remove(old_filepath)
 
        im.thumbnail((256, 1024))
        im.save("public/images/thumbnails/" + filename, "JPEG")
